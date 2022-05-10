cca9c073ba92595c640639b6537945e068dcebb5
dan-studio <danstudiocode@gmail.com>
2022-05-11 00:56:34 +0900

    register.js added

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.S2lide
 
+
 @app.route('/')
 def home():
     #로그인 상태에 따라 index 로딩시 상태변수 전달 / 로그인페이지 => 로그아웃으로 변경
     logged = False
     if "user_id" in session:
         logged = True
-    return render_template('main.html', logged = logged)
+    return render_template('register.html', logged = logged)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

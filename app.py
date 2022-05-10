94e5146176e473f660949155a098479fd9074526
dsyhwk1016 <kjsjsj1109@naver.com>
2022-05-10 11:58:54 +0900

    회원가입 구현 완료

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # app.py - main application
 
 from flask import Flask, render_template, session
+from bp.users import users
 
 #Flask App Setup
 app = Flask(__name__)
 
+# BluePrint Setup
+app.register_blueprint(users)   # 로그인 및 회원 가입
+
 @app.route('/')
 def home():
-    #로그인 상태에 따라 index 로딩시 상태변수 전달 / 로그인페이지 => 로그아웃으로 변경
-    logged = False
-    if "user_id" in session:
-        logged = True
-    return render_template('main.html', logged = logged)
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
